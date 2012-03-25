095107c11b1f147ac43ae419bb6fca4f5a3db1f1
Avdi Grimm <avdi@avdi.org>
2012-03-25 02:07:26 -0400

    Add Cowsay.new_cow

--- lib/cowsay.rb
+++ lib/cowsay.rb
@@ -1,6 +1,10 @@
 require 'pathname'
 
 module Cowsay
+  def self.new_cow(*args)
+    Cow.new(*args)
+  end
+
   class Cow
     def say(message)
       cowsay_path = Pathname(__FILE__).dirname + "../bin/cowsay"
--- spec/cowsay_integration_spec.rb
+++ spec/cowsay_integration_spec.rb
@@ -1,5 +1,7 @@
 require 'cowsay'
-describe Cowsay::Cow do
+describe 'a cow object' do
+  subject { Cowsay.new_cow }
+
   describe "#say" do
     it 'returns an ascii cow' do
       expected = (<<'END').strip
