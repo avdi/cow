ba7b7aac31f3cb397bf1d81f25032d07f12c37eb
Avdi Grimm <avdi@avdi.org>
2012-03-25 16:42:45 -0400

    Make service into modular Sinatra app.

--- lib/cow.rb
+++ lib/cow.rb
@@ -1,16 +1,18 @@
-require 'sinatra'
+require 'sinatra/base'
 require 'json'
 require_relative 'cowsay'
 
-get '/' do
-  content_type :txt
-  message = params.fetch('message') { 'Hello' }
-  cowfile = params.fetch('cowfile') { 'default'   }
-  cow = Cowsay.new_cow(:cowfile => cowfile)
-  cow.say(message)
-end
+class Cow < Sinatra::Base
+  get '/' do
+    content_type :txt
+    message = params.fetch('message') { 'Hello' }
+    cowfile = params.fetch('cowfile') { 'default'   }
+    cow = Cowsay.new_cow(:cowfile => cowfile)
+    cow.say(message)
+  end
 
-get '/cowfiles' do
-  content_type :json
-  Cowsay.cowfiles.to_json
+  get '/cowfiles' do
+    content_type :json
+    Cowsay.cowfiles.to_json
+  end
 end
--- spec/cow_integration_spec.rb
+++ spec/cow_integration_spec.rb
@@ -9,7 +9,7 @@ describe "The Cow app" do
   include Rack::Test::Methods
 
   def app
-    Sinatra::Application
+    Cow
   end
 
   def normalize_text(text)
@@ -94,8 +94,8 @@ END
         daemon default dragon dragon-and-cow duck elephant
         elephant-in-snake eyes flaming-sheep ghostbusters gnu head-in
         hellokitty kiss kitty koala kosh luke-koala mech-and-cow meow
-        milk moofasa moose mutilated pony pony-smaller ren sheep
-        skeleton snowman sodomized-sheep stegosaurus stimpy suse
+        milk moofasa moose pony pony-smaller ren sheep
+        skeleton snowman stegosaurus stimpy suse
         three-eyes turkey turtle tux unipony unipony-smaller vader
         vader-koala www
       ])
